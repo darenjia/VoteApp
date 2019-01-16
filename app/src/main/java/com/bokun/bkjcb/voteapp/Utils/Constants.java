c30438570e8696370aced69cb6021ca811e943fa
darenjia <12557532205@qq.com>
2019-01-16 15:27:00 +0800

    voteapp2.0

--- app/src/main/java/com/bokun/bkjcb/voteapp/Utils/Constants.java
+++ app/src/main/java/com/bokun/bkjcb/voteapp/Utils/Constants.java
@@ -17,17 +17,17 @@ public class Constants {
      *正式IP地址
      * */
 
-    public static final String URL_SOFT = "http://101.231.52.50:2047/votejk/voteapp.apk";
+    public static final String URL_SOFT = "http://192.168.200.18:2045/votejk/voteapp.apk";
 
     /*
      * 测试IP地址
      * */
     public static String TEST_HTTPURL = "http://192.168.100.4:2333/WebService.asmx";
-    public static String IMGURL = "http://101.231.52.50:2047";
+    public static String IMGURL = "http://192.168.200.18:2045";
     //public static String IMGURL = "http://192.168.100.192:80";
     public static final String URL_CHECK = "http://192.168.100.136:8080/zgzxjkWebService.asmx";
     //    public static final String URL_SOFT = "http://101.231.52.50:8081/Aqgl/xiazaiapp";
-    public static final String BASE_URL = "http://101.231.52.50:2046/vote/";
+    public static final String BASE_URL = "http://192.168.200.18:2048/vote/";
     //public static final String BASE_URL = "http://192.168.100.192:2046/vote/";
 
 }
